42ad75eee7d45806b335f3d1b02c6e4a17a98191
Ankan Biswas <ankanbiswas365@gmail.com>
2026-02-28 21:52:11 +0530

    added inngest for background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,20 @@
 import prisma from '@/lib/db';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { createTRPCRouter, protectedProcedure } from '../init';
+import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
-    getUsers: protectedProcedure.query(({ ctx }) => {
-        return prisma.user.findMany();
-        }),
+    getWorkflows: protectedProcedure.query(({ ctx }) => {
+        return prisma.workflow.findMany();
+    }),
+    createWorkflow: protectedProcedure.mutation(async () => {
+        await inngest.send({
+            name: "test/hello.world",
+            data: {
+                email: "ankan@gmail.com",
+            }
+        })
+
+        return {success: true, message: "Job queued"}
+    }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
