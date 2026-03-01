74ca7318eacb6721ae02f5b7eb9b68c7d1a35e09
Ankan Biswas <ankanbiswas365@gmail.com>
2026-03-02 00:41:15 +0530

    added polar for payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -9,5 +11,24 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
         autoSignIn: true,
-    }
+    },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "7a9cfcb0-081f-42e0-9434-35ac56b2d505",
+                            slug: "pro" // Custom slug for easy reference in Checkout URL, e.g. /checkout/Nodebase-Pro
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true
+                }),
+                portal()
+            ],
+        })
+    ]
 });
